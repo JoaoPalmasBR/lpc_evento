654c5983791d495f07ebd0ba5b7e191604c1738f
Joao Antonio Santos <jsantos737@users.noreply.github.com>
2017-08-28 21:48:30 -0300

    conteudo da aula 05 - dia 28-08-2017

--- eventos/admin.py
+++ eventos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from eventos.models import *
 # Register your models here.
+
+admin.site.register(Endereco)
+admin.site.register(Pessoa)
+admin.site.register(PessoaFisica)
+admin.site.register(Evento)
+admin.site.register(Inscricao)
--- eventos/models.py
+++ eventos/models.py
@@ -1,10 +1,47 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-
+class Endereco(models.Model):
+    logradouro = models.CharField(max_length=128)
+    complemento = models.CharField(max_length=256, null=True)
+    uf = models.CharField(max_length=2, null=True)
+    cidade = models.CharField(max_length=64, null=True)
+    cep = models.CharField(max_length=10)
+    def __str__(self):
+        return '{} - {}, {}'.format(self.logradouro, self.cidade, self.uf)
 class Pessoa(models.Model):
     nome = models.CharField(max_length=128)
     descricao = models.TextField()
     data_nascimento = models.DateField(blank=True, null=True)
+    endereco = models.ForeignKey(Endereco, related_name='pessoas', null=True, blank=False)
+    usuario = models.OneToOneField(User)
 
     def __str__(self):
         return self.nome
+class PessoaFisica(Pessoa):
+    cpf = models.CharField(max_length=128)
+    mae = models.CharField(max_length=128, null=True)
+    pai = models.CharField(max_length=128, null=True)
+
+    def __str__(self):
+        return '{} - {}'.format(self.nome, self.cpf)
+class Evento(models.Model):
+    nome = models.CharField(max_length=128)
+    descricao = models.CharField(max_length=256)
+    sigla = models.CharField(max_length=4)
+    numero = models.CharField(max_length=10)
+    ano = models.CharField(max_length=4)
+    realizador = models.ForeignKey(PessoaFisica, related_name='pessoasfisicas', null=True, blank=False)
+    endereco = models.ForeignKey(Endereco, null=True, blank=False)
+    logo = models.CharField(max_length=128)
+    data_de_inicio = models.DateField(blank=True, null=True)
+    data_de_fim = models.DateField(blank=True, null=True)
+    def __str__(self):
+        return '{} - {}'.format(self.nome, self.data_de_inicio)
+class Inscricao(models.Model):
+    evento = models.ForeignKey(Evento,  null=True, blank=False)
+    pessoa = models.ForeignKey(PessoaFisica, null=True, blank=False)
+    data_de_inscricao = models.DateTimeField(blank=True, null=True)
+    preco = models.FloatField()
+    def __str__(self):
+        return '{} - {}'.format(self.evento, self.pessoa)
